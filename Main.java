7e766cea8654b72e6bff52a8fc5f754c4ec2ecf7
hardik teotia <hardik10teotia@gmail.com>
2025-07-28 21:17:58 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+class Main {
+    public static void main(String[] args) {
+        int age = 30;
+        System.out.println("Hello, World!");
+        System.out.println(age);
+        Date currentDate = new Date();
+        System.out.println("Current date: " + currentDate);
+    }
+}
---
+++ array.java
@@ -0,0 +1,5 @@
+public class array {
+    public static void maine(str args[]){
+        int[] arr = new int[5];
+    }
+}
