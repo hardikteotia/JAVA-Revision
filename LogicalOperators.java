60e2f3644d083b04bdff7a466d91431c0f8fa1dd
hardik teotia <hardik10teotia@gmail.com>
2025-07-30 00:05:58 +0530

    third commit

---
+++ LogicalOperators.java
@@ -0,0 +1,7 @@
+public class LogicalOperators {
+    public static void main (String args[]){
+        int temp =20;
+        boolean tempIsWarm = temp < 30 || temp > 30;
+        System.out.println(tempIsWarm);
+    }
+}
