7553624e9bb2af07b4ea6a602ad53d0b2454286e
hardik teotia <hardik10teotia@gmail.com>
2025-08-02 21:39:04 +0530

    created a folder

---
+++ Ultimate Programming JAVA/Classes.java
@@ -0,0 +1,18 @@
+public class Classes {
+    public static void main(String[] args) {
+        
+    }
+
+}
+
+public class TextBox{
+    String text;
+
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+    public void clear(){
+        text = "";
+    }
+}
